1be29cac3c0ddfd15b46cfe160b753edaf757b1d
Hatice AKGÜL <katrancihatice@icloud.com>
2025-02-02 15:36:56 +0300

    Fix initial name

--- src/Pages/Calculate.js
+++ src/Pages/Calculate.js
@@ -32,7 +32,7 @@ function SlideTransition(props) {
 function Calculate() {
     const [inputCount, setInputCount] = useState("");
     const [credits, setcredits] = useState("");
-    const [initial, setInitialInput] = useState("");
+    const [initial, setInitial] = useState("");
     const [generatedRows, setGeneratedRows] = useState([]);
     const [irrValue, setIrrValue] = useState(null);
     const [tableData, setTableData] = useState([]);
@@ -54,7 +54,7 @@ function Calculate() {
     const handleInitialChange = (e) => {
         const value = e.target.value;
         if (/^\d*\.?\d*$/.test(value)) {
-            setInitialInput(value);
+            setInitial(value);
             dispatch(setInitial(value ? parseFloat(value) : null));
         }
     };
